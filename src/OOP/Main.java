fbb9a3522ca6296065618bc0cbf592dd55d9651a
Vladuca <bogdan_dima@ukr.net>
2026-04-30 14:57:05 +0300

    add_constructor

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -2,13 +2,8 @@ package OOP;
 
 public class Main {
 	public static void main(String[] arg) {
-		Rect rect = new Rect();
-		rect.setDimention(4, 4);
-		int a = rect.perimeter();
-		int b = rect.square();
-		System.out.print(a);
-		System.out.print(b);
-
-
+		Worker worker = new Worker("Петя", "Водій", 35000);
+		String saleryMonth = worker.workerinfo(2);
+		System.out.println(saleryMonth);
 	}
 }
